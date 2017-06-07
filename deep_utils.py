e5421db2b8f51ebd0d0e3053ab2bda9817c2b47d
Alex Hagen <ahagen@purdue.edu>
2017-06-06 22:17:19 -0400

    still not working because of weakref problems

--- deep_utils.py
+++ deep_utils.py
@@ -1,17 +1,30 @@
+import inspect
+
 def deep_hash(obj):
     """ deep_hash creates a hash from the object
 
     """
-    pass
+    objtree = tree(obj)
+    return objtree.hash()
 
 def deep_cmp(obj1, obj2):
     """ deep_cmp compares two objects deeply
 
     """
-    pass
+    obj1tree = tree(obj1)
+    obj2tree = tree(obj2)
+    return obj1tree.hash() == obj2tree.hash()
 
 class tree(object):
     """ a tree is an object's structure
     """
     def __init__(self, obj):
+        members = inspect.getmembers(obj)
+        print members
         pass
+
+    def string(self):
+        """ returns the string representation of the object """
+
+    def hash(self):
+        """ returns the hash of the object and all its """
